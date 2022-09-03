b9c1341a6cf3b8522317f80d9623edea21de437c
Darya <darecc@yandex.ru>
2022-09-03 22:09:05 +0400

    add Task 38

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -11,6 +11,6 @@
     }
 Console.WriteLine("[{0}]", String.Join(",",array));
 Console.WriteLine("max:  " + max);
-Console.WriteLine("min:  " +min);
+Console.WriteLine("min:  " + min);
 Console.WriteLine("Разница максимального и минимального:  "+(max - min));
         
